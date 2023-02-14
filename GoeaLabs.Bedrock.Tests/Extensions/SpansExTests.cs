e4b8346789c67bd094758c8cd9a28480ecde7ccd
FalxDraco <93272126+FalxDraco@users.noreply.github.com>
2023-02-14 09:50:24 -0500

    Got rid of Buffer.BlockCopy. Added guards.

--- GoeaLabs.Bedrock.Tests/Extensions/SpansExTests.cs
+++ GoeaLabs.Bedrock.Tests/Extensions/SpansExTests.cs
@@ -1,10 +1,21 @@
-﻿using GoeaLabs.Bedrock.Extensions;
+﻿/*
+   Copyright 2022, GoeaLabs
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+
+using GoeaLabs.Bedrock.Extensions;
 
 namespace GoeaLabs.Bedrock.Tests.Extensions
 {
@@ -27,7 +38,7 @@ namespace GoeaLabs.Bedrock.Tests.Extensions
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void Split_uints_to_bytes_throws_ArgumentException_if_input_too_large()
         {
             var max = int.MaxValue / sizeof(uint);
@@ -40,7 +51,7 @@ namespace GoeaLabs.Bedrock.Tests.Extensions
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void Split_uints_to_bytes_throws_ArgumentException_if_output_too_small()
         {
             Span<uint> uints = stackalloc uint[1];
@@ -64,8 +75,8 @@ namespace GoeaLabs.Bedrock.Tests.Extensions
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void Merge_bytes_to_uints_throws_ArgumentException_if_input_not_multiple_of_sizeof_uint()
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Merge_bytes_to_uints_throws_ArgumentOutOfRangeException_if_input_not_multiple_of_sizeof_uint()
         {
             Span<byte> bytes = stackalloc byte[7];
             Span<uint> uints = stackalloc uint[2];
@@ -74,13 +85,28 @@ namespace GoeaLabs.Bedrock.Tests.Extensions
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void Merge_bytes_to_uints_throws_ArgumentException_if_output_too_small()
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Merge_bytes_to_uints_throws_ArgumentOutOfRangeException_if_output_too_small()
         {
             Span<byte> bytes = stackalloc byte[8];
             Span<uint> uints = stackalloc uint[1];
 
             bytes.Merge(uints);
+        }
+
+        [TestMethod]
+        [DataRow(new byte[] { 0, 1 }, new byte[] { 0, 1, 2 })]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void UInt8span_XOR_throws_ArgumentOutOfRangeException_if_self_length_not_greater_than_that_length(byte[] self, byte[] that) 
+            => self.AsSpan().XOR(that);
+
+
+        [TestMethod]
+        [DataRow(new byte[] { 1, 2, 3, 4 }, new byte[] { 5, 6, 7, 8 }, new byte[] { 4, 4, 4, 12 })]
+        public void UInt8_span_XOR_behaves_correctly(byte[] self, byte[] that, byte[] okay)
+        {
+            self.AsSpan().XOR(that);
+            Assert.IsTrue(self.IsEqual(okay));
         }
     }
 }
--- GoeaLabs.Bedrock/Extensions/SpansEx.cs
+++ GoeaLabs.Bedrock/Extensions/SpansEx.cs
@@ -1,8 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+   Copyright 2022, GoeaLabs
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using CommunityToolkit.Diagnostics;
+
 
 namespace GoeaLabs.Bedrock.Extensions
 {
@@ -13,10 +26,28 @@ namespace GoeaLabs.Bedrock.Extensions
     {
 
         /// <summary>
+        /// Performs an element-by-element equality test between 
+        /// <paramref name="self"/> and <paramref name="that"/>.
+        /// </summary>
+        /// <param name="self">Compared array.</param>
+        /// <param name="that">Comparand array.</param>
+        /// <returns><b>True</b> if all elements are equal, otherwise <b>False</b>.</returns>
+        public static bool IsEqual(this Span<byte> self, Span<byte> that)
+        {
+            if (self.Length != that.Length)
+                return false;
+
+            for (int i = 0; i < self.Length; i++)
+                if (self[i] != that[i]) return false;
+
+            return true;
+        }
+
+        /// <summary>
         /// Writes the content of the Span to a Span of <see cref="byte"/>(s);
         /// </summary>
         /// <remarks>
-        /// Throws <see cref="ArgumentException"/>:
+        /// Throws <see cref="ArgumentOutOfRangeException"/>:
         /// <list type="bullet">
         /// <item>If the length of <paramref name="self"/> is greater than <c>int.MaxValue / sizeof(uint)</c></item>
         /// <item>If the length of <paramref name="that"/> is less than <c>self.Length * sizeof(uint)</c>.</item>
@@ -27,14 +58,10 @@ namespace GoeaLabs.Bedrock.Extensions
         public static void Split(this Span<uint> self, Span<byte> that)
         {
             var selfMax = int.MaxValue / sizeof(uint);
-
-            if (self.Length > selfMax)
-                throw new ArgumentException($"Length must be maximum {selfMax}", nameof(self));
+            Guard.IsLessThanOrEqualTo(self.Length, selfMax);
 
             var thatMin = self.Length * sizeof(uint);
-
-            if (that.Length < thatMin)
-                throw new ArgumentException($"Length must be minimum {thatMin}", nameof(that));
+            Guard.IsGreaterThanOrEqualTo(that.Length, thatMin);
 
             for (int i = 0; i < self.Length; i++)
             {
@@ -56,7 +83,7 @@ namespace GoeaLabs.Bedrock.Extensions
         /// Writes the content of the Span to a Span of <see cref="uint"/>(s);
         /// </summary>
         /// <remarks>
-        /// Throws <see cref="ArgumentException"/>:
+        /// Throws <see cref="ArgumentOutOfRangeException"/>:
         /// <list type="bullet">
         /// <item>If the length of <paramref name="self"/> is not equal to or multiple of <c>sizeof(uint)</c>.</item>
         /// <item>If the length of <paramref name="that"/> is less than <c>self.Length / sizeof(uint)</c>.</item>
@@ -65,14 +92,13 @@ namespace GoeaLabs.Bedrock.Extensions
         /// <param name="self">The span to split.</param>
         /// <param name="that">The span to write to.</param>
         public static void Merge(this Span<byte> self, Span<uint> that)
-        {
+        {         
             if (self.Length % sizeof(uint) > 0)
-                throw new ArgumentException($"Length must be a multiple of {sizeof(uint)}", nameof(self));
+                ThrowHelper.ThrowArgumentOutOfRangeException(
+                    nameof(self), $"Length must be a multiple of {sizeof(uint)}");
 
             var thatMin = self.Length / sizeof(uint);
-
-            if (that.Length < thatMin)
-                throw new ArgumentException($"Length must be minimum {thatMin}", nameof(that));
+            Guard.IsGreaterThanOrEqualTo(that.Length, thatMin);
 
             for (int i = 0; i < that.Length; i++)
             {
@@ -83,6 +109,26 @@ namespace GoeaLabs.Bedrock.Extensions
 
                 that[i] = n16a.Merge(n16b);               
             }
+        }
+
+        /// <summary>
+        /// <b>XOR</b>s each element of <paramref name="self"/> with the element at the same 
+        /// index in <paramref name="that"/> and writes the results in <paramref name="self"/>.
+        /// </summary>
+        /// <remarks>
+        /// Throws <see cref="ArgumentOutOfRangeException"/>:
+        /// <list type="bullet">
+        /// <item>If <paramref name="self"/> has more elements than <paramref name="that"/>.</item>
+        /// </list>
+        /// </remarks>
+        /// <param name="self">The array to operate on.</param>
+        /// <param name="that">The array to XOR with.</param>
+        public static void XOR(this Span<byte> self, Span<byte> that)
+        {
+            Guard.IsGreaterThanOrEqualTo(self.Length, that.Length);
+
+            for (int i = 0; i < self.Length; i++)
+                self[i] = (byte)(self[i] ^ that[i]);
         }
     }
 }
