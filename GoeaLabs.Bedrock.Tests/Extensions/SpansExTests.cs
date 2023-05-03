5687c785be4f757f1a9042918741f1531b987a08
FalxDraco <93272126+FalxDraco@users.noreply.github.com>
2023-05-03 17:23:21 -0400

    API update and code clean-up.

--- GoeaLabs.Bedrock.Tests/Extensions/SpansExTests.cs
+++ GoeaLabs.Bedrock.Tests/Extensions/SpansExTests.cs
@@ -24,29 +24,14 @@ using GoeaLabs.Bedrock.Extensions;
 
 namespace GoeaLabs.Bedrock.Tests.Extensions
 {
-
     [TestClass]
     public class SpansExTests
     {
-
-        [TestMethod]
-        [DataRow(
-            new [] { 0xFFFFFFFF }, 
-            new byte[] { 0xFF, 0xFF, 0xFF, 0xFF })]
-        [DataRow(
-            new uint[] { 0xFFFFFFFF, 0x0000FFFF }, 
-            new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF })]
-        public void Split_uints_to_bytes_behaves_correctly(uint[] data, byte[] okay)
-        {
-            Span<byte> test = stackalloc byte[data.Length * sizeof(uint)];
-            new Span<uint>(data).Split(test);
-
-            Assert.IsTrue(test.SequenceEqual(okay));
-        }
+        #region Split
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Split_uints_to_bytes_throws_ArgumentException_if_input_too_large()
+        public void Split_UInt32s_to_UInt8s_throws_ArgumentException_if_incorrect_input_length()
         {
             var max = int.MaxValue / sizeof(uint);
             var arr = new uint[++max];
@@ -59,7 +44,7 @@ namespace GoeaLabs.Bedrock.Tests.Extensions
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Split_uints_to_bytes_throws_ArgumentException_if_output_too_small()
+        public void Split_UInt32s_to_UInt8s_throws_ArgumentException_if_incorrect_output_length()
         {
             Span<uint> uints = stackalloc uint[1];
             Span<byte> bytes = stackalloc byte[1];
@@ -69,47 +54,126 @@ namespace GoeaLabs.Bedrock.Tests.Extensions
 
         [TestMethod]
         [DataRow(
-            new byte[] { 0xFF, 0xFF, 0xFF, 0xFF }, 
-            new [] { 0xFFFFFFFF })]
+            new[] { 0xFFFFFFFF },
+            new byte[] { 0xFF, 0xFF, 0xFF, 0xFF })]
         [DataRow(
-            new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF }, 
-            new uint[] { 0xFFFFFFFF, 0x0000FFFF })]
-        public void Merge_bytes_to_uints_behaves_correctly(byte[] data, uint[] okay)
+            new uint[] { 0xFFFFFFFF, 0x0000FFFF },
+            new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF })]
+        public void Split_UInt32s_to_UInt8s_outputs_correct_results(uint[] data, byte[] okay)
+        {
+            Span<byte> test = stackalloc byte[data.Length * sizeof(uint)];
+            new Span<uint>(data).Split(test);
+
+            Assert.IsTrue(test.SequenceEqual(okay));
+        }
+
+        #endregion
+
+        #region Merge
+
+        [TestMethod]
+        [DataRow(3, 2)]
+        [DataRow(5, 2)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Merge_UInt8s_to_UInt32s_throws_ArgumentException_if_incorrect_input_length(int srcLen, int outLen)
+        {
+            Span<byte> source = stackalloc byte[srcLen];
+            Span<uint> output = stackalloc uint[outLen];
+            
+            source.Merge(output);
+        }
+        
+        [TestMethod]
+        [DataRow(4, 5)]
+        [DataRow(8, 3)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Merge_UInt8s_to_UInt32s_throws_ArgumentException_if_incorrect_output_length(int srcLen, int outLen)
+        {
+            Span<byte> source = stackalloc byte[srcLen];
+            Span<uint> output = stackalloc uint[outLen];
+            
+            source.Merge(output);
+        }
+
+        [TestMethod]
+        [DataRow(
+            new byte[]
+            {
+                0xFF, 0xFF, 0xFF, 0xFF
+            },
+            new[] { 0xFFFFFFFF })]
+        [DataRow(
+            new byte[]
+            {
+                0xFF, 0xFF, 0xFF, 0xFF, 
+                0x00, 0x00, 0xFF, 0xFF
+            },
+            new uint[]
+            {
+                0xFFFFFFFF, 0x0000FFFF
+            })]
+        public void Merge_UInt8s_to_UInt32s_outputs_correct_results(byte[] data, uint[] okay)
         {
             Span<uint> test = stackalloc uint[data.Length / sizeof(uint)];
             new Span<byte>(data).Merge(test);
+
+            Assert.IsTrue(test.SequenceEqual(okay));
+        }
+
+        [TestMethod]
+        [DataRow(7, 2)]
+        [DataRow(9, 2)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Merge_UInt8s_to_UInt64s_throws_ArgumentException_if_incorrect_input_length(int srcLen, int outLen)
+        {
+            Span<byte> source = stackalloc byte[srcLen];
+            Span<uint> output = stackalloc uint[outLen];
             
-            Assert.IsTrue(test.SequenceEqual(okay)); 
+            source.Merge(output);
         }
-
+        
         [TestMethod]
+        [DataRow(16, 1)]
+        [DataRow(32, 5)]
         [ExpectedException(typeof(ArgumentException))]
-        public void Merge_bytes_to_uints_throws_ArgumentException_if_incorrect_input_length()
+        public void Merge_UInt8s_to_UInt64s_throws_ArgumentException_if_incorrect_output_length(int srcLen, int outLen)
         {
-            Span<byte> bytes = stackalloc byte[7];
-            Span<uint> uints = stackalloc uint[2];
-
-            bytes.Merge(uints);
+            Span<byte> source = stackalloc byte[srcLen];
+            Span<uint> output = stackalloc uint[outLen];
+            
+            source.Merge(output);
         }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void Merge_bytes_to_uints_throws_ArgumentException_if_incorrect_output_length()
-        {
-            Span<byte> bytes = stackalloc byte[8];
-            Span<uint> uints = stackalloc uint[1];
-
-            bytes.Merge(uints);
-        }
-
+        
         [TestMethod]
         [DataRow(
-            new [] { 0xFFFFFFFF, 0xFFFFFFFF }, 
-            new [] { 0xFFFFFFFFFFFFFFFF })]
+            new byte[]
+            {
+                0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+            },
+            new[] { 0xFFFFFFFFFFFFFFFF })]
         [DataRow(
-            new [] { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFE }, 
-            new [] { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFE })]
-        public void Merge_uints_to_ulongs_behaves_correctly(uint[] data, ulong[] okay)
+            new byte[]
+            {
+                0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF
+            },
+            new[] { 0xFFFFFFFFFFFFFFFF, 0x00000000FFFFFFFF })]
+        public void Merge_UInt8s_to_UInt64s_outputs_correct_results(byte[] data, ulong[] okay)
+        {
+            Span<ulong> test = stackalloc ulong[data.Length / sizeof(ulong)];
+            new Span<byte>(data).Merge(test);
+
+            Assert.IsTrue(test.SequenceEqual(okay));
+        }
+        
+        [TestMethod]
+        [DataRow(
+            new[] { 0xFFFFFFFF, 0xFFFFFFFF },
+            new[] { 0xFFFFFFFFFFFFFFFF })]
+        [DataRow(
+            new[] { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFE },
+            new[] { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFE })]
+        public void Merge_UInt32s_to_UInt64s_outputs_correct_results(uint[] data, ulong[] okay)
         {
             Span<uint> dataS = data;
 
@@ -123,62 +187,34 @@ namespace GoeaLabs.Bedrock.Tests.Extensions
         [DataRow(1, 1)]
         [DataRow(3, 1)]
         [ExpectedException(typeof(ArgumentException))]
-        public void Merge_uints_to_ulongs_throws_ArgumentException_if_incorrect_input_length(int szSelf, int szThat)
+        public void Merge_Uint32s_to_UInt64s_throws_ArgumentException_if_incorrect_input_length(int srcLen, int outLen)
         {
-            Span<uint> span32 = stackalloc uint[szSelf];
-            Span<ulong> span64 = stackalloc ulong[szThat];
+            Span<uint> source = stackalloc uint[srcLen];
+            Span<ulong> output = stackalloc ulong[outLen];
 
-            span32.Merge(span64);
+            source.Merge(output);
         }
 
         [TestMethod]
         [DataRow(4, 1)]
         [DataRow(8, 3)]
         [ExpectedException(typeof(ArgumentException))]
-        public void Merge_uints_to_ulongs_throws_ArgumentException_if_incorrect_output_length(int szSelf, int szThat)
+        public void Merge_UInt32s_to_UInt64s_throws_ArgumentException_if_incorrect_output_length(int srcLen, int outLen)
         {
-            Span<uint> span32 = stackalloc uint[szSelf];
-            Span<ulong> span64 = stackalloc ulong[szThat];
+            Span<uint> source = stackalloc uint[srcLen];
+            Span<ulong> output = stackalloc ulong[outLen];
 
-            span32.Merge(span64);
+            source.Merge(output);
         }
 
         [TestMethod]
-        [DataRow(new []
-        {
-            0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF
-        })]
-        [DataRow(new []
-            {
-                0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 
-                0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFE
-            })]
-        public void Merge_uints_to_uint128s_behaves_correctly(uint[] data)
-        {
-            Span<UInt128> test = stackalloc UInt128[data.Length / 4];
-            new Span<uint>(data).Merge(test);
-
-            switch (data.Length)
-            {
-                case 4:
-                    Assert.IsTrue(test.SequenceEqual(new [] { UInt128.MaxValue }));
-                    break;
-                case 8:
-                    Assert.IsTrue(test.SequenceEqual(new [] { UInt128.MaxValue, UInt128.MaxValue - 1 }));
-                    break;
-                default:
-                    throw new ArgumentException("Length must be either 4 or 8.", nameof(data));
-            }
-        }
-
-        [TestMethod]
-        [DataRow(1, 1)]
-        [DataRow(5, 1)]
+        [DataRow(3, 1)]
+        [DataRow(5, 2)]
         [ExpectedException(typeof(ArgumentException))]
-        public void Merge_uints_to_uint128s_throws_ArgumentException_if_incorrect_input_length(int szSelf, int szThat)
+        public void Merge_UInt32s_to_UInt128s_throws_ArgumentException_if_incorrect_input_length(int srcLen, int outLen)
         {
-            Span<uint> span32 = stackalloc uint[szSelf];
-            Span<UInt128> span128 = stackalloc UInt128[szThat];
+            Span<uint> span32 = stackalloc uint[srcLen];
+            Span<UInt128> span128 = stackalloc UInt128[outLen];
 
             span32.Merge(span128);
         }
@@ -187,28 +223,66 @@ namespace GoeaLabs.Bedrock.Tests.Extensions
         [DataRow(8, 1)]
         [DataRow(16, 3)]
         [ExpectedException(typeof(ArgumentException))]
-        public void Merge_uints_to_uint128s_throws_ArgumentException_if_incorrect_output_length(int szSelf, int szThat)
+        public void Merge_UInt32s_to_UInt128s_throws_ArgumentException_if_incorrect_output_length(int szSelf, int szThat)
         {
             Span<uint> span32 = stackalloc uint[szSelf];
             Span<UInt128> span128 = stackalloc UInt128[szThat];
 
             span32.Merge(span128);
         }
+        
+        [TestMethod]
+        [DataRow(new[]
+        {
+            0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF
+        })]
+        [DataRow(new[]
+        {
+            0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF,
+            0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFE
+        })]
+        public void Merge_UInt32s_to_UInt128128s_outputs_correct_results(uint[] data)
+        {
+            Span<UInt128> test = stackalloc UInt128[data.Length / 4];
+            new Span<uint>(data).Merge(test);
 
+            switch (data.Length)
+            {
+                case 4:
+                    Assert.IsTrue(test.SequenceEqual(new[] { UInt128.MaxValue }));
+                    break;
+                case 8:
+                    Assert.IsTrue(test.SequenceEqual(new[] { UInt128.MaxValue, UInt128.MaxValue - 1 }));
+                    break;
+                default:
+                    throw new ArgumentException("Length must be either 4 or 8.", nameof(data));
+            }
+        }
+        
+        #endregion
+
+        #region Other
 
         [TestMethod]
-        [DataRow(new byte[] { 0, 1, 2 }, new byte[] { 0, 1 })]
+        [DataRow(
+            new byte[] { 0, 1, 2 },
+            new byte[] { 0, 1 })]
         [ExpectedException(typeof(ArgumentException))]
-        public void UInt8span_XOR_throws_ArgumentException_if_incorrect_lengths(byte[] self, byte[] that) 
-            => self.AsSpan().Xor(that);
+        public void UInt8span_XOR_throws_ArgumentException_if_incorrect_lengths(byte[] self, byte[] that) =>
+            self.AsSpan().Xor(that);
 
 
         [TestMethod]
-        [DataRow(new byte[] { 1, 2, 3, 4 }, new byte[] { 5, 6, 7, 8 }, new byte[] { 4, 4, 4, 12 })]
-        public void UInt8_span_Xor_behaves_correctly(byte[] self, byte[] that, byte[] okay)
+        [DataRow(
+            new byte[] { 1, 2, 3, 4 },
+            new byte[] { 5, 6, 7, 8 },
+            new byte[] { 4, 4, 4, 12 })]
+        public void UInt8_span_Xor_outputs_correct_results(byte[] self, byte[] that, byte[] okay)
         {
             self.AsSpan().Xor(that);
-            Assert.IsTrue(self.IsEqual(okay));
+            Assert.IsTrue(self.SequenceEqual(okay));
         }
+
+        #endregion
     }
 }
--- GoeaLabs.Bedrock/Extensions/SpansEx.cs
+++ GoeaLabs.Bedrock/Extensions/SpansEx.cs
@@ -21,6 +21,7 @@
  */
 
 
+using System.Runtime.CompilerServices;
 using CommunityToolkit.Diagnostics;
 
 
@@ -28,48 +29,31 @@ namespace GoeaLabs.Bedrock.Extensions
 {
 
     /// <summary>
-    /// Extensions for integer spans.
+    /// Extensions for unsigned integer spans.
     /// </summary>
+    [SkipLocalsInit]
     public static class SpansEx
     {
-
-        /// <summary>
-        /// Performs an element-by-element equality test between 
-        /// <paramref name="self"/> and <paramref name="that"/>.
-        /// </summary>
-        /// <param name="self">Compared array.</param>
-        /// <param name="that">Comparand array.</param>
-        /// <returns><b>True</b> if all elements are equal, otherwise <b>False</b>.</returns>
-        public static bool IsEqual(this Span<byte> self, Span<byte> that)
-        {
-            if (self.Length != that.Length)
-                return false;
-
-            for (var i = 0; i < self.Length; i++)
-                if (self[i] != that[i]) return false;
-
-            return true;
-        }
-
+        
+        #region Split
+        
         /// <summary>
         /// Writes the content of a span of <see cref="uint"/>(s) to a span of <see cref="byte"/>(s).
         /// </summary>
-        /// <remarks>
-        /// Throws <see cref="ArgumentException"/>:
-        /// <list type="bullet">
-        /// <item>If the length of <paramref name="self"/> is greater than <c>int.MaxValue / sizeof(uint)</c>.</item>
-        /// <item>If the length of <paramref name="that"/> is less than <c>self.Length * sizeof(uint)</c>.</item>
-        /// </list>
-        /// </remarks>
-        /// <param name="self">The span to split.</param>
-        /// <param name="that">The span to write to.</param>
+        /// <param name="self">Source span.</param>
+        /// <param name="that">Output span.</param>
+        /// <exception cref="ArgumentException">
+        /// If the length of <paramref name="self"/> is greater than <c>int.MaxValue / sizeof(uint)</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If the length of <paramref name="that"/> is not equal to <c>self.Length * sizeof(uint)</c>.
+        /// </exception>
         public static void Split(this Span<uint> self, Span<byte> that)
         {
-            const int selfMax = int.MaxValue / sizeof(uint);
-            Guard.HasSizeLessThanOrEqualTo(self, selfMax);
-
-            var thatMin = self.Length * sizeof(uint);
-            Guard.HasSizeGreaterThanOrEqualTo(that, thatMin);
+            const int size = sizeof(uint);
+            
+            Guard.HasSizeLessThanOrEqualTo(self, int.MaxValue / size);
+            Guard.HasSizeEqualTo(that, self.Length * size);
 
             for (var i = 0; i < self.Length; i++)
             {
@@ -78,42 +62,45 @@ namespace GoeaLabs.Bedrock.Extensions
                 n16A.Halve(out var n8A, out var n8B);
                 n16B.Halve(out var n8C, out var n8D);
 
-                var spot = i * sizeof(uint);
+                var spot = i * size;
 
                 that[spot++] = n8A;
                 that[spot++] = n8B;
                 that[spot++] = n8C;
+                // This warning is blatantly incorrect
                 // ReSharper disable once RedundantAssignment
                 that[spot++] = n8D;
             }
         }
-
+        
+        #endregion
+        
+        #region Merge
+        
         /// <summary>
         /// Writes the content of a span of <see cref="byte"/>(s) to a span of <see cref="uint"/>(s).
         /// </summary>
-        /// <remarks>
-        /// Throws <see cref="ArgumentException"/>:
-        /// <list type="bullet">
-        /// <item>If the length of <paramref name="self"/> is a multiple of <c>sizeof(uint)</c>.</item>
-        /// <item>If the length of <paramref name="that"/> is less than <c>self.Length / sizeof(uint)</c>.</item>
-        /// </list>
-        /// </remarks>
-        /// <param name="self">The span to split.</param>
-        /// <param name="that">The span to write to.</param>
+        /// <param name="self">Source span.</param>
+        /// <param name="that">Output span.</param>
+        /// <exception cref="ArgumentException">
+        /// If the length of <paramref name="self"/> is not a multiple of <c>sizeof(uint)</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If the length of <paramref name="that"/> is not equal to <c>self.Length / sizeof(uint)</c>.
+        /// </exception>
         public static void Merge(this Span<byte> self, Span<uint> that)
         {
-            const int quot = sizeof(uint);
-
-            if (self.Length % quot > 0)
+            const int size = sizeof(uint);
+            
+            if (self.Length % size > 0)
                 ThrowHelper.ThrowArgumentException(
-                    nameof(self), $"Length must be a multiple of {quot}");
-
-            Guard.HasSizeGreaterThanOrEqualTo(that, self.Length / quot);
+                    nameof(self), $"Length must be a multiple of {size}");
+            
+            Guard.HasSizeEqualTo(that, self.Length / size);
 
             for (var i = 0; i < that.Length; i++)
             {
-                var spot = i * quot;
-
+                var spot = i * size;
                 var n16A = self[spot].Merge(self[++spot]);
                 var n16B = self[++spot].Merge(self[++spot]);
 
@@ -122,30 +109,56 @@ namespace GoeaLabs.Bedrock.Extensions
         }
 
         /// <summary>
+        /// Writes the content of a span of <see cref="byte"/>(s) to a span of <see cref="ulong"/>(s).
+        /// </summary>
+        /// <param name="self">Source span.</param>
+        /// <param name="that">Output span.</param>
+        /// <exception cref="ArgumentException">
+        /// If the length of <paramref name="self"/> is not a multiple of <c>sizeof(ulong)</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If the length of <paramref name="that"/> is not equal to <c>self.Length / sizeof(ulong)</c>.
+        /// </exception>
+        public static void Merge(this Span<byte> self, Span<ulong> that)
+        {
+            const int size = sizeof(ulong);
+            
+            if (self.Length % size > 0)
+                ThrowHelper.ThrowArgumentException(
+                    nameof(self), $"Length must be a multiple of {size}");
+            
+            Guard.HasSizeEqualTo(that, self.Length / size);
+
+            Span<uint> buff = stackalloc uint[self.Length / sizeof(uint)];
+            
+            self.Merge(buff);
+            buff.Merge(that);
+        }
+
+        /// <summary>
         /// Writes the content of a span of <see cref="uint"/>(s) to a span of <see cref="ulong"/>(s).
         /// </summary>
-        /// <remarks>
-        /// Throws <see cref="ArgumentException"/>:
-        /// <list type="bullet">
-        /// <item>If the length of <paramref name="self"/> is not a multiple of <c>sizeof(ulong) / sizeof(uint)</c>.</item>
-        /// <item>If the length of <paramref name="that"/> is less than <c>self.Length / (sizeof(ulong) / sizeof(uint))</c>.</item>
-        /// </list>
-        /// </remarks>
-        /// <param name="self">The span to split.</param>
-        /// <param name="that">The span to write to.</param>
+        /// <param name="self">Source span.</param>
+        /// <param name="that">Output span.</param>
+        /// <exception cref="ArgumentException">
+        /// If the length of <paramref name="self"/> is not a multiple of <c>sizeof(ulong) / sizeof(uint)</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If the length of <paramref name="that"/> is not equal to <c>self.Length / (sizeof(ulong) / sizeof(uint))</c>.
+        /// </exception>
         public static void Merge(this Span<uint> self, Span<ulong> that)
         {
-            const int quot = sizeof(ulong) / sizeof(uint);
+            const int size = sizeof(ulong) / sizeof(uint);
 
-            if (self.Length % quot > 0)
+            if (self.Length % size > 0)
                 ThrowHelper.ThrowArgumentException(
-                    nameof(self), $"Length must be a multiple of {quot}");
+                    nameof(self), $"Length must be a multiple of {size}");
 
-            Guard.HasSizeGreaterThanOrEqualTo(that, self.Length / quot);
+            Guard.HasSizeEqualTo(that, self.Length / size);
 
             for (var i = 0; i < that.Length; i++)
             {
-                var spot = i * quot;
+                var spot = i * size;
 
                 that[i] = self[spot].Merge(self[++spot]);
             }
@@ -156,58 +169,66 @@ namespace GoeaLabs.Bedrock.Extensions
         /// <summary>
         /// Writes the content of a span of <see cref="uint"/>(s) to a span of <see cref="UInt128"/>(s).
         /// </summary>
-        /// <remarks>
-        /// Throws <see cref="ArgumentException"/>:
-        /// <list type="bullet">
-        /// <item>If the length of <paramref name="self"/> is not a multiple of <c>16 / sizeof(uint)</c>.</item>
-        /// <item>If the length of <paramref name="that"/> is less than <c>self.Length / (16 / sizeof(uint))</c>.</item>
-        /// </list>
-        /// </remarks>
-        /// <param name="self">The span to split.</param>
-        /// <param name="that">The span to write to.</param>
-        public static void Merge(this Span<uint> self, Span<UInt128> that)
-        {
-            var quot = 16 / sizeof(uint);
-
-            if (self.Length % quot > 0)
-                ThrowHelper.ThrowArgumentException(
-                    nameof(self), $"Length must be a multiple of {quot}");
-
-            Guard.HasSizeGreaterThanOrEqualTo(that, self.Length / quot);
-
-            for (var i = 0; i < that.Length; i++)
-            {
-                var spot = i * quot;
-
-                var n64A = self[spot].Merge(self[++spot]);
-                var n64B = self[++spot].Merge(self[++spot]);
-
-                that[i] = n64A.Merge(n64B);
-            }
-        }
-
-#endif
-
-        /// <summary>
-        /// <b>XOR</b>s <paramref name="self"/> with <paramref name="that"/> 
-        /// and writes the results in <paramref name="self"/>.
-        /// </summary>
+        /// <param name="self">Source span.</param>
+        /// <param name="that">Output span.</param>
         /// <remarks>
         /// Throws <see cref="ArgumentException"/>:
         /// <list type="bullet">
         /// <item>
-        /// If <paramref name="self"/> has more elements than <paramref name="that"/>.
+        /// If the length of <paramref name="self"/> is not a multiple of <c>sizeof(UInt128) / sizeof(uint)</c>.
+        /// </item>
+        /// <item>
+        /// If the length of <paramref name="that"/> is not equal to <c>self.Length / (sizeof(UInt128) / sizeof(uint))</c>.
         /// </item>
         /// </list>
         /// </remarks>
+        public static void Merge(this Span<uint> self, Span<UInt128> that)
+        {
+            unsafe
+            {
+               var quot = sizeof(UInt128) / sizeof(uint);
+
+                if (self.Length % quot > 0)
+                    ThrowHelper.ThrowArgumentException(
+                        nameof(self), $"Length must be a multiple of {quot}");
+
+                Guard.HasSizeEqualTo(that, self.Length / quot);
+
+                for (var i = 0; i < that.Length; i++)
+                {
+                    var spot = i * quot;
+                    var n64A = self[spot].Merge(self[++spot]);
+                    var n64B = self[++spot].Merge(self[++spot]);
+
+                    that[i] = n64A.Merge(n64B);
+                }
+            }
+        }
+
+#endif
+        
+        #endregion
+
+        #region Other
+        
+        /// <summary>
+        /// <b>Xor(s)</b>s <paramref name="self"/> with <paramref name="that"/> and writes the results to
+        /// <paramref name="self"/>.
+        /// </summary>
         /// <param name="self">The span to operate on.</param>
         /// <param name="that">The span to XOR with.</param>
+        /// <exception cref="ArgumentException">
+        /// If <paramref name="self"/> and <paramref name="that"/> don't have the same length.
+        /// </exception>
         public static void Xor(this Span<byte> self, Span<byte> that)
         {
-            Guard.HasSizeGreaterThanOrEqualTo(that, self.Length);
+            Guard.HasSizeEqualTo(that, self.Length);
 
             for (var i = 0; i < self.Length; i++)
                 self[i] = (byte)(self[i] ^ that[i]);
         }
+
+        #endregion
+        
     }
 }
